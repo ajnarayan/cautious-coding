a0648f34c0442eb9b69c4d37fe866555668e103b
ajnarayan <anjjan.narayan@yahoo.in>
2017-04-16 14:15:21 -0400

    Completed, need to test

--- AStar_Search/Board.java
+++ AStar_Search/Board.java
@@ -1,7 +1,8 @@
+import java.util.Stack;
 public class Board {
+    
     private final int size; 
     private final int[][] board;
-    
     
     public Board(int[][] blocks)   {        // construct a board from an n-by-n array of blocks
         // (where blocks[i][j] = block in row i, column j)
@@ -31,10 +32,9 @@ public class Board {
             for(int j=0; j<size; j++)
             if(board[i][j]!=0 && board[i][j] != (1 + i*size+j) ){
             //int distance = Math.abs(x1-x0) + Math.abs(y1-y0);
-            x0 = (board[i][j]) / dimension();
-            y0 = (board[i][j]) % dimentsion();
-            man += Math.abs(i-x0) + Math.abs(i+y0);
-            
+            int x0 = (board[i][j]-1) / dimension();
+            int y0 = (board[i][j]-1) % dimension();
+            man += Math.abs(i-x0) + Math.abs(j-y0);
         }
         return man;
     }
@@ -64,88 +64,88 @@ public class Board {
     }
     
     private boolean swap(int i, int j , int k, int l){
-        if(i>0 || i<= dimension() || j>0 || j<=dimension() ){
+ 
             int temp = board[i][j];
-                board[i][j] = board[i][j+1];
-                board[i][j+1] = temp;
-                return true; 
-        }
-        else
-            return false; 
+            board[i][j] = board[k][l];
+            board[k][l] = temp;
+            return true; 
     }
     
-public boolean equals(Object y) {       // does this board equal y?
-    //from http://algs4.cs.princeton.edu/12oop/Date.java.html
-    if(this == y)
-        return true;
-    if (this == null)
-        return false;
-    if (y.getClass() != this.getClass()) return false;
-    Board that = (Board) y;
-    return this.toString().equals(that.toString());   
-}  
-
-private int[] findVal(int val){
-    int ij = {0,0};
-    boolean flag = false;
-    for(int i=0; i<dimension(); i++) 
-        for(int j=0; j<dimension()-1; j++){
-        if(board[i][j] ==val) {
+    public boolean equals(Object y) {       // does this board equal y?
+        //from http://algs4.cs.princeton.edu/12oop/Date.java.html
+        if(this == y)
+            return true;
+        if (this == null)
+            return false;
+       
+        Board that = (Board) y;
+        if (this.size != that.size)
+            return false;
+        
+        return this.toString().equals(that.toString());   
+    }  
+    
+    public Iterable<Board> neighbors(){     // all neighboring boards
+        // To reduce unnecessary exploration of useless search nodes, when considering the
+        //neighbors of a search node, don't enqueue a neighbor if its board is the same as the board of the previous search node.   
+        Stack<Board> boardStack = new Stack<Board>();
+        int k =0, l=0;
+        boolean flag = false;
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board.length; j++){
+            if(board[i][j] ==0) {
+                k = i;
+                l = j;
+                break;
+            }
             
-         ij[0] = i;
-         ij[1] = j;
-         flag = true;
-         break;
         }
-              
+        }
+        Board b1 = new Board(board);
+        if(k-1 >=0){
+        boolean neighbor = b1.swap(k, l, k -1, l);
+        if(neighbor)
+            boardStack.push(b1);
+        }
+        
+        b1 = new Board(board);
+        if (l-1 >=0){
+        boolean neighbor = b1.swap(k, l, k, l-1);
+        if(neighbor)
+            boardStack.push(b1);
+        }
+        
+        b1 = new Board(board);
+        if(k+1< board.length){
+        boolean neighbor = b1.swap(k, l, k+1, l);
+        if(neighbor)
+            boardStack.push(b1);
+        }
+        
+        b1 = new Board(board);
+        if(l+1<board.length){
+        boolean neighbor = b1.swap(k, l, k, l+1);
+        if(neighbor)
+            boardStack.push(b1);
+        }
+        
+        return boardStack;
+        
     }
-    if(flag)
-        break; 
-}
-
-
-public Iterable<Board> neighbors(){     // all neighboring boards
- // To reduce unnecessary exploration of useless search nodes, when considering the
-    //neighbors of a search node, don't enqueue a neighbor if its board is the same as the board of the previous search node.   
-    Stack<Board> boardStack = new Stack<Board>();
-    int[] ij = findVal(0);
-    Board b1 = new Board(board);
-    boolean neighbor = b1.swap(ij[0], ij[1], ij[0] -1, ij[1]);
-    if(neighbor)
-        boardStack.push(b1);
     
-    b1 = new Board(board);
-    boolean neighbor = b1.swap(ij[0], ij[1], ij[0], ij[1]-1);
-    if(neighbor)
-        boardStack.push(b1);
-    
-    b1 = new Board(board);
-    boolean neighbor = b1.swap(ij[0], ij[1], ij[0]+1, ij[1]);
-    if(neighbor)
-        boardStack.push(b1);
-    
-    b1 = new Board(board);
-    boolean neighbor = b1.swap(ij[0], ij[1], ij[0], ij[1]+1);
-    if(neighbor)
-        boardStack.push(b1);
-    
-    return b1;
-    
-}
     public String toString()  {             // string representation of this board (in the output format specified below)
-    StringBuilder s = new StringBuilder();
-    s.append(n + "\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            s.append(String.format("%2d ", tiles[i][j]));
+        StringBuilder s = new StringBuilder();
+        s.append(size + "\n");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                s.append(String.format("%2d ", board[i][j]));
+            }
+            s.append("\n");
         }
-        s.append("\n");
+        return s.toString();  
     }
-    return s.toString();
     
-    
-}
     public static void main(String[] args){ // unit tests (not graded)
         
     }
-    }
+}
--- AStar_Search/Solver.java
+++ AStar_Search/Solver.java
@@ -1,9 +1,163 @@
 import edu.princeton.cs.algs4.MinPQ;
+import java.util.Stack;
+import java.util.Comparator;
+
+/*
+ * The success of this approach hinges on the choice of priority function for a search node. 
+ * We consider two priority functions: Hamming priority function & Manhattan priority function.
+ * 
+ * To solve the puzzle from a given search node on the priority queue, 
+ * the total number of moves we need to make (including those already made) is at least its priority,
+ * using either the Hamming or Manhattan priority function. (For Hamming priority, 
+ * this is true because each block that is out of place must move at least once to reach its goal position. 
+ * For Manhattan priority, this is true because each block must move its Manhattan distance from its goal position.
+ * Note that we do not count the blank square when computing the Hamming or Manhattan priorities.) Consequently, when the goal board is dequeued,
+ * we have discovered not only a sequence of moves from the initial board to the goal board, but one that makes the fewest number of moves. 
+ */
+
+
 
 public class Solver {
-    public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
-    public boolean isSolvable()            // is the initial board solvable?
-    public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
-    public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
-    public static void main(String[] args) // solve a slider puzzle (given below)
+    private MinPQ<SearchNode> priorityQue; 
+    private MinPQ<SearchNode> priorityQueTwin;
+    private boolean solved = true;
+    private Stack<Board> stackBoard;
+    
+    
+    //Solver Constructor
+    public Solver(Board initial)  {         // find a solution to the initial board (using the A* algorithm)
+        if (initial == null)
+            throw new NullPointerException();
+        solved = true;
+        priorityQue = new MinPQ<SearchNode>();
+        priorityQueTwin = new MinPQ<SearchNode>();
+         stackBoard = new Stack<Board>();
+        Board board = initial;
+        if(board.isGoal()){
+            solved = true;
+            this.stackBoard.push(board);
+            return;
+        }
+        Board twinBoard = initial.twin();
+        if(twinBoard.isGoal()){
+            solved = false;
+            return;
+        }
+        //First, insert the initial search node (the initial board, 0 moves, and a null previous search node) into a priority queue.
+        SearchNode node = new SearchNode(board,0,null); 
+        priorityQue.insert(node);
+        SearchNode twinNode = new SearchNode(twinBoard,0,null);
+        priorityQueTwin.insert(twinNode);
+        boolean solve = false;
+        while(!solve){
+            node = priorityQue.delMin();
+            board = node.board;
+            
+            
+            twinNode = priorityQueTwin.delMin();
+            twinBoard = twinNode.board;
+            
+            if(board.isGoal()){
+                solved = true;
+                solve = true;
+                this.stackBoard.push(board);
+                while(node.prevSearchNode != null){
+                    node = node.prevSearchNode;   
+                    stackBoard.push(node.board);
+                }
+                //solved
+                return;     
+            }
+            
+            //same as above for twin
+            if(twinBoard.isGoal()){
+                solved = false;
+                solve = true;
+                //cannot be solved
+                return;     
+            }
+            node.numOfMoves++;
+            twinNode.numOfMoves++;
+       
+        
+        //insert onto the priority queue all neighboring search nodes (those that can be reached in one move from the dequeued search node).
+        //Repeat this procedure until the search node dequeued corresponds to a goal board.
+        Iterable<Board> neighbor = board.neighbors();
+        //for node:
+        for(Board b : neighbor){
+            if(node.prevSearchNode == null && !b.equals(node.prevSearchNode.board)){
+            
+                SearchNode n = new SearchNode(b, node.numOfMoves, node);
+             priorityQue.insert(n);
+            }
+            
+        }
+        
+        //Same for twin
+        Iterable<Board> tneighbor = twinBoard.neighbors();
+        //for node:
+        for(Board b : tneighbor){
+            if(twinNode.prevSearchNode == null && !b.equals(twinNode.prevSearchNode.board)){
+           
+                SearchNode n = new SearchNode(b, twinNode.numOfMoves, twinNode);
+             priorityQueTwin.insert(n);
+            }
+            
+        }
+  
+    
+         }//end of while
+    }
+    
+    
+    public boolean isSolvable() {           // is the initial board solvable?
+        return solved;   
+        
+    }
+    public int moves()  {
+        // min number of moves to solve initial board; -1 if unsolvable
+        if(!solved)
+            return -1;
+        else
+            return stackBoard.size()-1;
+    }
+    
+    
+    
+    public Iterable<Board> solution()  {    // sequence of boards in a shortest solution; null if unsolvable
+        
+     if(!solved)
+         return null;
+     else
+         return stackBoard;
+
+    }
+    
+    private class SearchNode implements Comparator<SearchNode>{
+        //Search node of the game to be a board, 
+        //the number of moves made to reach the board, and the previous search node.
+        private  Board board; 
+        private  int numOfMoves;
+        private SearchNode prevSearchNode;
+        private int priority = -1;
+        
+        public SearchNode(Board board, int numOfMoves, SearchNode searchNode){
+            this.board = board;
+            this.numOfMoves = numOfMoves;
+            this.prevSearchNode = searchNode;
+        }
+  
+        //create comparator of hamming vs manhattan    
+        @Override 
+        public int compare(SearchNode a, SearchNode b){
+            return a.board.manhattan() + a.numOfMoves - b.board.manhattan() - b.numOfMoves;
+        }
+    }//end of SearchNode Class
+    
+    
+    
+    
+    public static void main(String[] args) {// solve a slider puzzle (given below)
+        
+    }
 }
